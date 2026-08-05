864e6db14a2807caeb7740bdb430f35b45690460
divjot1495 <div1495@gmail.com>
2026-08-05 17:32:25 +0530

    Revert "ui issues fixed"
    This reverts commit 40fea2cd998e9c9173621b6317b0b04368de158f.

--- components/layout/AppShell.tsx
+++ components/layout/AppShell.tsx
@@ -6,10 +6,9 @@ import BottomNavigation from "./BottomNavigation";
 
 interface Props {
   children: ReactNode;
-  mainClass?: string;
 }
 
-export default function AppShell({ children, mainClass }: Props) {
+export default function AppShell({ children }: Props) {
   return (
     <div className="bg-background flex min-h-screen">
       {/* Desktop only */}
@@ -20,9 +19,7 @@ export default function AppShell({ children, mainClass }: Props) {
       <div className="flex min-w-0 flex-1 flex-col">
         <TopBar />
 
-        <main className={`mt-16 flex-1 px-4 py-4 pb-2 ${mainClass ?? ""}`}>
-          {children}
-        </main>
+        <main className="mt-16 flex-1 px-4 py-4 pb-2">{children}</main>
 
         {/* Mobile only */}
         <div className="fixed inset-x-0 bottom-0 z-50 lg:hidden">
